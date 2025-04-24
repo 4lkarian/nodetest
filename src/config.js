cdf6a7bc4714f92e34f63f67acb2ee50d22cbb73
4lkarian <94425721+4lkarian@users.noreply.github.com>
2025-04-23 17:28:41 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000;
 
-export const DB_HOST = process.env.MYSQLHOST || 'localhost';
-export const DB_USER = process.env.MYSQLUSER || 'root';
-export const DB_PASSWORD = process.env.MYSQLPASSWORD || 'root';
-export const DB_NAME = process.env.MYSQLDATABASE || 'test';
-export const DB_PORT = process.env.MYSQLPORT || 3306;
+export const MYSQLHOST = process.env.MYSQLHOST || 'localhost';
+export const MYSQLUSER = process.env.MYSQLUSER || 'root';
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || 'root';
+export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'test';
+export const MYSQLPORT = process.env.MYSQLPORT || 3306;
