9e0cd53d6d048f268004a63c64ad4f32a9543087
4lkarian <94425721+4lkarian@users.noreply.github.com>
2025-04-24 14:07:23 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 import { pool } from './db.js';
 import { PORT } from './config.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
